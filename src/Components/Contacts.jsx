41206b73697bf62791d5b00ba09fa5c9973bebf4
Sagar Regmi <regmi.sagar@outlook.com>
2024-05-27 21:09:50 +0300

    Responsive error fixed

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
-import { WavyBackground } from './ui/wavy-background'
 import { FaMapMarkerAlt,FaPhone,FaEnvelope } from 'react-icons/fa'
 const Contacts = () => {
   return (
-    <div>
-      <WavyBackground>
+    <div className="h-min-screen bg-neutral-900 pt-24">
+        <h1 className="lg:text-6xl text-5xl font-bold text-white dark:text-white text-center mb-10">
+            👋 Hey, Let's discuss?
+        </h1>
+         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 lg:gap-12 gap-6 px-12">
+            <div className="flex-1 bg-gray-200 p-4 rounded-md shadow-md">
+                <div className="flex items-center space-x-2 mb-4">
+                    <div className="bg-yellow-500 text-black px-3 py-1 rounded-lg flex items-center justify-center text-xl font-bold w-10 h-10">
+                        1
+                    </div>
+                    <p className="font-bold">You arrange a phone or email conversation via contact details.</p>
+                </div>
+            </div>
+            <div className="flex-1 bg-gray-200 p-4 rounded-md shadow-md">
+                <div className="flex items-center space-x-2 mb-4">
+                    <div className="bg-yellow-500 text-black px-3 py-1 rounded-lg flex items-center justify-center text-xl font-bold w-10 h-10">
+                        2
+                    </div>
+                    <p className="font-bold">I will conduct an exploratory meeting.</p>
+                </div>
+            </div>
+            <div className="flex-1 bg-gray-200 p-4 rounded-md shadow-md">
+                <div className="flex items-center space-x-2 mb-4">
+                    <div className="bg-yellow-500 text-black px-3 py-1 rounded-lg flex items-center justify-center text-xl font-bold w-10 h-10">
+                        3
+                    </div>
+                    <p className="font-bold">I will prepare a work proposal.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full mt-8 mx-auto">
+            <h2 className="text-2xl font-bold mb-6 text-center">Contact Details</h2>
+            <div className="flex items-center mb-4 justify-center">
+                <FaEnvelope className="text-yellow-500 mr-4" />
+                <span className="text-gray-700">iamregmisagar@gmail.com</span>
+            </div>
+            <div className="flex items-center mb-4 justify-center">
+                <FaPhone className="text-yellow-500 mr-4" />
+                <span className="text-gray-700">+358 453272287</span>
+            </div>
+            <div className="flex items-center justify-center">
+                <FaMapMarkerAlt className="text-yellow-500 mr-4" />
+                <span className="text-gray-700">Porslahdentie 24 Helsinki Finland</span>
+            </div>
+        </div>
+
+    </div>
+      /* <WavyBackground>
         <h1 className="text-6xl font-bold text-white dark:text-white text-center mb-10">
             Lets Work Together?
         </h1>
@@ -51,8 +97,8 @@ const Contacts = () => {
                 <span className="text-gray-700">Porslahdentie 24 Helsinki Finland</span>
             </div>
         </div>
-    </WavyBackground>
-    </div>
+    </WavyBackground> */
+    
   )
 }
 
--- src/Components/IntroSection.jsx
+++ src/Components/IntroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import regmiImg from "../Content/regmi.png"
 const IntroSection = () => {
   return (
-    <div className="section-hero bg-neutral-900 p-8 md:p-20 h-[70vh] md:-[70vh] lg:h-[65vh] mt-20 md:mt-5 lg:mt-0">
+    <div className="section-hero bg-neutral-900 p-8 md:p-20 h-[70vh] md:-[70vh] lg:h-[65vh] mt-14 md:mt-5 lg:mt-0">
     <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-center">
       <div className="text-center md:w-1/2 md:text-left text-white mb-8 md:mb-0">
         <h1 className="text-3xl md:text-4xl lg:text-5xl mb-2"><strong>👋</strong></h1>
